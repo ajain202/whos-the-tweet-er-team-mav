fff10a05d0062974a927ea0056e5b47ef552590a
aakash jain <92187222+ajain202@users.noreply.github.com>
2022-12-02 20:49:51 -0600

    token encryption for secure api calls

--- functions/get-tweets-for-following.ts
+++ functions/get-tweets-for-following.ts
@@ -1,30 +1,41 @@
 import { Handler } from '@netlify/functions';
 import { TwitterApi } from 'twitter-api-v2';
 import { FollowingTweets } from '../src/models/models';
+import { decryptData } from '../src/utilities/encryption';
 
 const handler: Handler = async (event, _context) => {
   try {
-    const client = new TwitterApi({
-      appKey: process.env.TWITTER_API_KEY || '',
-      appSecret: process.env.TWITTER_API_SECRET || '',
-      accessToken: event.queryStringParameters.accessToken || '',
-      accessSecret: event.queryStringParameters.accessSecret || '',
-    });
-
-    const followingList: Array<string> = event.queryStringParameters.following?.split(',') || [];
-    const followingTweets: Array<FollowingTweets> = [];
-
-    for (const following of followingList) {
-      const { data: tweets } = await client.v2.get(`users/${following}/tweets`, {
-        max_results: 100,
-        exclude: 'retweets,replies',
+    const { accessToken, accessSecret, following, eat } = JSON.parse(
+      decryptData(event.queryStringParameters.token),
+    );
+    if (eat && eat > Date.now()) {
+      const client = new TwitterApi({
+        appKey: process.env.TWITTER_API_KEY || '',
+        appSecret: process.env.TWITTER_API_SECRET || '',
+        accessToken: accessToken || '',
+        accessSecret: accessSecret || '',
       });
-      followingTweets.push({ following, tweets: tweets ? tweets : [] });
+
+      const followingList: Array<string> = following?.split(',') || [];
+      const followingTweets: Array<FollowingTweets> = [];
+
+      for (const following of followingList) {
+        const { data: tweets } = await client.v2.get(`users/${following}/tweets`, {
+          max_results: 100,
+          exclude: 'retweets,replies',
+        });
+        followingTweets.push({ following, tweets: tweets ? tweets : [] });
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(followingTweets),
+      };
+    } else {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ error: 'Unauthorized access' }),
+      };
     }
-    return {
-      statusCode: 200,
-      body: JSON.stringify(followingTweets),
-    };
   } catch (_error) {
     return {
       statusCode: 500,
